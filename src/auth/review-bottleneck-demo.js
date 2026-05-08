8318cbec80f4b6c4b8df94ec3bf0288307d0dc29
youngsun <77915491+dudtjs1021ej@users.noreply.github.com>
2026-05-08 21:28:16 +0900

    Add Sprinty PR demo note #1

---
+++ src/auth/review-bottleneck-demo.js
@@ -0,0 +1,5 @@
+export const sprintyReviewDemo = {
+  issue: "#1",
+  scenario: "PR is open while the GitHub Issue still looks Todo",
+  expectedSprintyQuestion: "Should the board move to Review?"
+};
